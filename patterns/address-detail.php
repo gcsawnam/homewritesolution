9d3591f81829f283a4854ca6351852de73e36d46
rameshdevkota23 <devkotaramesh79@gmail.com>
2024-01-07 14:01:21 +0545

    banner pattern

--- patterns/address-detail.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Title: Address Detail
- * Slug: featured/address-detail
- * Categories: featured
- */
-?>
-
-
---
+++ patterns/banner.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Title: Banner
+ * Slug: featured/banner
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|60"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"},"color":{"text":"#25373f"},"elements":{"link":{"color":{"text":"#25373f"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#25373f;font-style:normal;font-weight:500;line-height:1.2">Everything You Need For</h2>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":1,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.2","fontSize":"50px"},"color":{"text":"#ff541e"},"elements":{"link":{"color":{"text":"#ff541e"}}}}} -->
+<h1 class="wp-block-heading has-text-color has-link-color" style="color:#ff541e;font-size:50px;font-style:normal;font-weight:700;line-height:1.2">Consistent Online Engagement</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"level":5,"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}},"color":{"text":"#25373f"},"elements":{"link":{"color":{"text":"#25373f"}}}}} -->
+<h5 class="wp-block-heading has-text-color has-link-color" style="color:#25373f;margin-top:var(--wp--preset--spacing--50)">Under One Roof</h5>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--50)"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":11811,"width":"43px","height":"43px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2024/01/dev-icon01.png" alt="" class="wp-image-11811" style="object-fit:cover;width:43px;height:43px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"extra-small"} -->
+<h5 class="wp-block-heading has-text-align-center has-extra-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4">Website<br>Development</h5>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":11824,"width":"43px","height":"43px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2024/01/social-icon.png" alt="" class="wp-image-11824" style="object-fit:cover;width:43px;height:43px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"extra-small"} -->
+<h5 class="wp-block-heading has-text-align-center has-extra-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4">Social Media<br>Marketing</h5>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":11825,"width":"43px","height":"43px","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2024/01/seo-icon.png" alt="" class="wp-image-11825" style="object-fit:cover;width:43px;height:43px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"extra-small"} -->
+<h5 class="wp-block-heading has-text-align-center has-extra-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4">Search Engine<br>Optimization</h5>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":11824,"width":"43px","height":"auto","aspectRatio":"1","scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/corporate/wp-content/uploads/2024/01/social-icon.png" alt="" class="wp-image-11824" style="aspect-ratio:1;object-fit:cover;width:43px;height:auto"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":5,"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.4"}},"fontSize":"extra-small"} -->
+<h5 class="wp-block-heading has-text-align-center has-extra-small-font-size" style="font-style:normal;font-weight:500;line-height:1.4">Social Media<br>and Google Ads</h5>
+<!-- /wp:heading --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|50"}},"color":{"text":"#25373f"},"elements":{"link":{"color":{"text":"#25373f"}}},"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1.2"}}} -->
+<h4 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#25373f;margin-top:var(--wp--preset--spacing--50);font-style:normal;font-weight:600;line-height:1.2">Let’s Get<br>On A Discovery Call</h4>
+<!-- /wp:heading -->
+
+<!-- wp:shortcode -->
+[contact-form-7 id="e1f3728" title="Contact form 1"]
+<!-- /wp:shortcode --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
