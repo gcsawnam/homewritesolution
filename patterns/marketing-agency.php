422404272d2fcdd1a2e3dd72175fb63cd9ae5095
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-01-08 12:41:55 +0545

    updated

---
+++ patterns/marketing-agency.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Title: Marketing Agency
+ * Slug: featured/marketing-agency
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"25px","fontStyle":"normal","fontWeight":"500","lineHeight":"1.2"}}} -->
+<h2 class="wp-block-heading" style="font-size:25px;font-style:normal;font-weight:500;line-height:1.2">360° Digital Marketing Agency Nepal</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"color":{"text":"#777777"},"elements":{"link":{"color":{"text":"#777777"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#777777;font-size:18px;font-style:normal;font-weight:400;line-height:1.8">As a leading Digital marketing agency in Nepal, we are committed to delivering brand-specific, audience targeted and conversion oriented SEO, Web Design, Social media marketing, Business branding &amp; Online reputation management services to our clients to meet their Digital marketing goals. We call ourselves the innovative &amp; problem-solving firm that gets your business to the next level.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":1588,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/infographic.jpg" alt="" class="wp-image-1588"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":"50px"} -->
+<div style="height:50px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":1591,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/conversion-rate-optimization-1.png" alt="" class="wp-image-1591"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","lineHeight":"1.2","fontSize":"35px"},"color":{"text":"#333333"},"elements":{"link":{"color":{"text":"#333333"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#333333;font-size:35px;font-style:normal;font-weight:500;line-height:1.2">We Don’t just Bring Traffic;<br>We Believe in Conversion</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"color":{"text":"#777777"},"elements":{"link":{"color":{"text":"#777777"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#777777;font-size:18px;font-style:normal;font-weight:400;line-height:1.8">Welcome to Digital Terai, one of the top digital marketing agency in Nepal helping clients boost their sales &amp; leads and grow their businesses online. Located at Kathmandu, Nepal, We thrive to produce a tangible high ROI campaign for our clients. We love to work closely with clients so that we can better get to know their business model, requirements &amp; their expectations in order to plan our strategies accordingly.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
