373f58a7bf3bb472664ee971fddd7a848c97c5a8
gcsawnam <gcsawnam@gmail.com>
2024-01-10 10:10:38 +0545

    fixes

---
+++ patterns/header.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: Header
+ * Slug: featured/header
+ * Categories: featured
+ */
+?>
+
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)"><!-- wp:site-logo {"width":175} /-->
+
+<!-- wp:navigation {"ref":4,"textColor":"black","style":{"typography":{"fontSize":"15px","fontStyle":"normal","fontWeight":"500"}}} /-->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"background","textColor":"black","style":{"typography":{"fontSize":"16px"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"border":{"color":"#c9c9c9","width":"1px","radius":"6px"},"spacing":{"padding":{"left":"15px","right":"15px","top":"9px","bottom":"9px"}}},"className":"nav-button"} -->
+<div class="wp-block-button has-custom-font-size nav-button" style="font-size:16px"><a class="wp-block-button__link has-black-color has-background-background-color has-text-color has-background has-link-color has-border-color wp-element-button" style="border-color:#c9c9c9;border-width:1px;border-radius:6px;padding-top:9px;padding-right:15px;padding-bottom:9px;padding-left:15px">+91 981 845 6688</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
