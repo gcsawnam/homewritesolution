aef0daa70991e2ffb7d8386e579cbad95597085f
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-01-11 16:53:56 +0545

    updated

---
+++ patterns/team.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Title: Team 
+ * Slug: featured/team
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"color":{"text":"#012c7c"},"elements":{"link":{"color":{"text":"#012c7c"}}}},"fontSize":"medium"} -->
+<p class="has-text-align-center has-text-color has-link-color has-medium-font-size" style="color:#012c7c">Meet our team</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"color":{"text":"#404040"},"elements":{"link":{"color":{"text":"#404040"}}}},"fontSize":"very-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color has-very-large-font-size" style="color:#404040;font-style:normal;font-weight:700">TEAM MEMBERS</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"layout":{"type":"constrained","contentSize":"75%"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"elements":{"link":{"color":{"text":"#757575"}}},"color":{"text":"#757575"}},"fontSize":"medium"} -->
+<p class="has-text-align-center has-text-color has-link-color has-medium-font-size" style="color:#757575;font-style:normal;font-weight:400">Do you want to meet awesome members of Orka Socials? Our talented niche-specific experts are all the way into making your business great. Follow them on their social media to look at their updates.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"48px"} -->
+<div style="height:48px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"className":"team-first","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group team-first" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--60)"><!-- wp:image {"id":1813,"width":"auto","height":"325px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/testimonial.jpg" alt="" class="wp-image-1813" style="width:auto;height:325px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#444444"},"elements":{"link":{"color":{"text":"#444444"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#444444;font-size:20px;font-style:normal;font-weight:600;line-height:1">Pratab Bc</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#1868fe"},"elements":{"link":{"color":{"text":"#1868fe"}}}},"fontSize":"medium"} -->
+<p class="has-text-color has-link-color has-medium-font-size" style="color:#1868fe;font-style:normal;font-weight:600;line-height:1">SEO Strategist / Team Lead</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30","padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}}},"className":"team-second","layout":{"type":"constrained"}} -->
+<div class="wp-block-group team-second" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#444444"},"elements":{"link":{"color":{"text":"#444444"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#444444;font-size:20px;font-style:normal;font-weight:600;line-height:1">Pratab Bc</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#1868fe"},"elements":{"link":{"color":{"text":"#1868fe"}}}},"fontSize":"medium"} -->
+<p class="has-text-color has-link-color has-medium-font-size" style="color:#1868fe;font-style:normal;font-weight:600;line-height:1">SEO Strategist / Team Lead</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"color":{"text":"#757575"},"elements":{"link":{"color":{"text":"#757575"}}},"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"fontSize":"medium"} -->
+<p class="has-text-color has-link-color has-medium-font-size" style="color:#757575;margin-top:var(--wp--preset--spacing--30);font-style:normal;font-weight:400;line-height:1.8">A website built on a poor foundation can lead to destructive impacts even though it has excellent content and backlinks. When optimizing the overall infrastructures of websites, Orka is proud of Praphulla. He looks at technical SEO as your foundation. He has a wide range of skill sets optimizing technical aspects of SEO.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:social-links {"customIconBackgroundColor":"#1868fe","iconBackgroundColorValue":"#1868fe"} -->
+<ul class="wp-block-social-links has-icon-background-color"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"#","service":"twitter"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":1813,"width":"auto","height":"325px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/testimonial.jpg" alt="" class="wp-image-1813" style="width:auto;height:325px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#444444"},"elements":{"link":{"color":{"text":"#444444"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#444444;font-size:20px;font-style:normal;font-weight:600;line-height:1">Pratab Bc</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#1868fe"},"elements":{"link":{"color":{"text":"#1868fe"}}}},"fontSize":"medium"} -->
+<p class="has-text-color has-link-color has-medium-font-size" style="color:#1868fe;font-style:normal;font-weight:600;line-height:1">SEO Strategist / Team Lead</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":1813,"width":"auto","height":"325px","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full is-resized"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/testimonial.jpg" alt="" class="wp-image-1813" style="width:auto;height:325px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#444444"},"elements":{"link":{"color":{"text":"#444444"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#444444;font-size:20px;font-style:normal;font-weight:600;line-height:1">Pratab Bc</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1"},"color":{"text":"#1868fe"},"elements":{"link":{"color":{"text":"#1868fe"}}}},"fontSize":"medium"} -->
+<p class="has-text-color has-link-color has-medium-font-size" style="color:#1868fe;font-style:normal;font-weight:600;line-height:1">SEO Strategist / Team Lead</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
---
+++ patterns/testimonial.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: Testimonial 
+ * Slug: featured/testimonial
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"42px","fontStyle":"normal","fontWeight":"900","lineHeight":"1.5"},"color":{"text":"#59616f"},"elements":{"link":{"color":{"text":"#59616f"}}}}} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#59616f;font-size:42px;font-style:normal;font-weight:900;line-height:1.5">What our Customers Say....</h2>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"12px"} -->
+<div style="height:12px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator {"className":"is-style-default"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-default"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:video {"id":1741} -->
+<figure class="wp-block-video"><video controls src="http://localhost/wordpress/wp-content/uploads/2024/01/transport_-_23232-720p-online-video-cutter.com_.mp4"></video></figure>
+<!-- /wp:video --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:video {"id":1741} -->
+<figure class="wp-block-video"><video controls src="http://localhost/wordpress/wp-content/uploads/2024/01/transport_-_23232-720p-online-video-cutter.com_.mp4"></video></figure>
+<!-- /wp:video --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:video {"id":1741} -->
+<figure class="wp-block-video"><video controls src="http://localhost/wordpress/wp-content/uploads/2024/01/transport_-_23232-720p-online-video-cutter.com_.mp4"></video></figure>
+<!-- /wp:video --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
