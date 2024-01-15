c7d4256da836f430a78d0ed057b4d8bc7b464efb
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-01-15 12:06:54 +0545

    create pattern for contact page

---
+++ patterns/contact.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Title: Client
+ * Slug: featured/client
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"color":{"background":"#003859"},"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#003859;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.1"},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"textColor":"background"} -->
+<p class="has-background-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:700;line-height:1.1">CONTACT US</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"var:preset|color|background"}}},"typography":{"fontSize":"48px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"}},"textColor":"background","className":"contact-ready"} -->
+<h2 class="wp-block-heading contact-ready has-background-color has-text-color has-link-color" style="font-size:48px;font-style:normal;font-weight:700;line-height:1.2">Are you <mark style="background-color:#701d40" class="has-inline-color has-background-color">ready to start?</mark></h2>
+<!-- /wp:heading --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","level":3,"style":{"color":{"text":"#ba0c2f"},"elements":{"link":{"color":{"text":"#ba0c2f"}}},"typography":{"fontSize":"32px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.3"}}} -->
+<h3 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#ba0c2f;font-size:32px;font-style:normal;font-weight:700;line-height:1.3">Let us drum up business for you.</h3>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","level":3,"style":{"color":{"text":"#736d6c"},"elements":{"link":{"color":{"text":"#736d6c"}}},"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"1.2","fontSize":"26px"}}} -->
+<h3 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#736d6c;font-size:26px;font-style:normal;font-weight:400;line-height:1.2">You'll be surprised at our quick response!</h3>
+<!-- /wp:heading -->
+
+<!-- wp:spacer {"height":"19px"} -->
+<div style="height:19px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.4"},"color":{"text":"#736d6c"},"elements":{"link":{"color":{"text":"#ba0c2f"},":hover":{"color":{"text":"#f7f7f7"}}}}},"className":"contact-link"} -->
+<p class="has-text-align-center contact-link has-text-color has-link-color" style="color:#736d6c;font-size:18px;font-style:normal;font-weight:400;line-height:1.4"><a href="#">Fill out the form below</a> to let our full in-house team of brand experts, writers, SEO strategists, web designers, developers, and videographers take you to the next level.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"19px"} -->
+<div style="height:19px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"color":{"background":"#f4f3f3"}},"className":"call-box","layout":{"type":"default"}} -->
+<div class="wp-block-group call-box has-background" style="background-color:#f4f3f3"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"26px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.2"},"color":{"text":"#382f2d"},"elements":{"link":{"color":{"text":"#382f2d"}}}}} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#382f2d;font-size:26px;font-style:normal;font-weight:400;line-height:1.2">Or call us to get a quick response</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"27px","fontStyle":"normal","fontWeight":"700"},"color":{"text":"#ba0c2f"},"elements":{"link":{"color":{"text":"#ba0c2f"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#ba0c2f;font-size:27px;font-style:normal;font-weight:700"><a href="#">864.254.6098</a></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.6"},"color":{"text":"#736d6c"},"elements":{"link":{"color":{"text":"#736d6c"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#736d6c;font-size:16px;font-style:normal;font-weight:400;line-height:1.6">Mon -Fri | 8am - 5pm</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.4"},"elements":{"link":{"color":{"text":"#736d6c"},":hover":{"color":{"text":"#cc3366"}}}}}} -->
+<p class="has-text-align-left has-link-color" style="font-size:18px;font-style:normal;font-weight:400;line-height:1.4"><a href="#">35 Cessna Court, Suite C Greenville, SC 29607</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"19px"} -->
+<div style="height:19px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:embed {"url":"https://www.youtube.com/watch?time_continue=1\u0026v=MW-X1oYFNe8\u0026embeds_referring_euri=https%3A%2F%2Fdrumcreative.com%2F\u0026embeds_referring_origin=https%3A%2F%2Fdrumcreative.com\u0026feature=emb_title","type":"video","providerNameSlug":"youtube","responsive":true,"className":"wp-embed-aspect-16-9 wp-has-aspect-ratio"} -->
+<figure class="wp-block-embed is-type-video is-provider-youtube wp-block-embed-youtube wp-embed-aspect-16-9 wp-has-aspect-ratio"><div class="wp-block-embed__wrapper">
+https://www.youtube.com/watch?time_continue=1&amp;v=MW-X1oYFNe8&amp;embeds_referring_euri=https%3A%2F%2Fdrumcreative.com%2F&amp;embeds_referring_origin=https%3A%2F%2Fdrumcreative.com&amp;feature=emb_title
+</div></figure>
+<!-- /wp:embed --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
