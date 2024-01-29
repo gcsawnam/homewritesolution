8bc3554da0cf6018cba6900b73a1c294675182f7
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-01-29 13:54:42 +0545

    fixed

---
+++ patterns/friend-banner.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Title: Friend Banner
+ * Slug: featured/friend-banner
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"color":{"background":"#f0f6ff"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group has-background" style="background-color:#f0f6ff"><!-- wp:spacer {"height":"48px"} -->
+<div style="height:48px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","padding":{"right":"0","left":"0","top":"0","bottom":"0"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|40"}}} -->
+<div class="wp-block-column"><!-- wp:image {"id":2300,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/wave.png" alt="" class="wp-image-2300"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"style":{"typography":{"fontSize":"42px","fontStyle":"normal","fontWeight":"900","lineHeight":"1.2"},"color":{"text":"#2e2e2e"},"elements":{"link":{"color":{"text":"#2e2e2e"}}}}} -->
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#2e2e2e;font-size:42px;font-style:normal;font-weight:900;line-height:1.2">BECOME A PART FROM OUR <mark style="background-color:rgba(0, 0, 0, 0);color:#408eff" class="has-inline-color">REFERRAL PROGRAM</mark></h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"friend-list","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group friend-list"><!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","lineHeight":"1.7"},"color":{"text":"#474747"},"elements":{"link":{"color":{"text":"#474747"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#474747;font-size:18px;font-style:normal;font-weight:600;line-height:1.7">If you have any colleagues or friends who would benefit from our services, please refer them to us by providing information about the business you're referring to (company name, telephone number, and contact person).</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"friend-list","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group friend-list"><!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","lineHeight":"1.7","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#474747"},"elements":{"link":{"color":{"text":"#474747"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#474747;font-size:18px;font-style:normal;font-weight:600;line-height:1.7">You'll receive a<mark style="background-color:rgba(0, 0, 0, 0);color:#408eff" class="has-inline-color"> $400 Visa GIft Card</mark> once your referral hires White Shark Media.*</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"friend-list","layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"top"}} -->
+<div class="wp-block-group friend-list"><!-- wp:paragraph {"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","lineHeight":"1.7"},"color":{"text":"#474747"},"elements":{"link":{"color":{"text":"#474747"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#474747;font-size:18px;font-style:normal;font-weight:600;line-height:1.7">If the referral is not immediately onboarded, you'll get updates on the referral's status for the first three months and then on a quarterly basis until the referral chooses to hire our srvices.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"16px"} -->
+<div style="height:16px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"700"},"color":{"background":"#408eff"},"border":{"radius":"30px"}}} -->
+<div class="wp-block-button has-custom-font-size" style="font-size:16px;font-style:normal;font-weight:700"><a class="wp-block-button__link has-background wp-element-button" style="border-radius:30px;background-color:#408eff">Refer a Friend</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":2258,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/wordpress/wp-content/uploads/2024/01/friend-reffer-banner.png" alt="" class="wp-image-2258"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"48px"} -->
+<div style="height:48px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer --></div>
+<!-- /wp:group -->
---
+++ patterns/friend-contact-more.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Friend Contact More
+ * Slug: featured/friend-contact-more
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained","contentSize":"50%"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"34px","fontStyle":"normal","fontWeight":"900","lineHeight":"1.2"},"color":{"text":"#2e2e2e"},"elements":{"link":{"color":{"text":"#2e2e2e"}}}}} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#2e2e2e;font-size:34px;font-style:normal;font-weight:900;line-height:1.2"><mark style="background-color:rgba(0, 0, 0, 0);color:#408eff" class="has-inline-color">CONTACT US </mark>FOR MORE INFORMATION</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"3px"} -->
+<div style="height:3px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"83%"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","lineHeight":"1.7"},"color":{"text":"#474747"},"elements":{"link":{"color":{"text":"#474747"}}}}} -->
+<p class="has-text-align-center has-text-color has-link-color" style="color:#474747;font-size:18px;font-style:normal;font-weight:600;line-height:1.7">You’re welcome to reach out at <mark style="background-color:rgba(0, 0, 0, 0);color:#408eff" class="has-inline-color">info@whitesharkmedia.com</mark> or <a href="#"><mark style="background-color:rgba(0, 0, 0, 0);color:#408eff" class="has-inline-color">(305) 728-4828</mark></a> if you have any questions about our Referral Program.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained","contentSize":"55%"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600","lineHeight":"1.8"},"color":{"text":"#474747"},"elements":{"link":{"color":{"text":"#474747"}}}}} -->
+<p class="has-text-align-center has-text-color has-link-color" style="color:#474747;font-size:12px;font-style:normal;font-weight:600;line-height:1.8">A Visa gift card will be granted once your referral has remained our client for at least three months. See out terms and conditions for all the details</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
