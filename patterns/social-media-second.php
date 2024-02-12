7114a9cc05a4f1d2b815bffcbfaf3f30a59ce4e9
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-02-12 17:01:04 +0545

    updated

---
+++ patterns/social-media-second.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Title: Social Media Second
+ * Slug: featured/social-media-second
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:cover {"url":"http://localhost/wordpress/wp-content/uploads/2024/02/sucess-2.webp","id":2831,"dimRatio":50,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-2831" alt="" src="http://localhost/wordpress/wp-content/uploads/2024/02/sucess-2.webp" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":2826,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/cva.png" alt="" class="wp-image-2826"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"style":{"spacing":{"blockGap":"0"}}} -->
+<div class="wp-block-column"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":"1.1"},"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
+<h2 class="wp-block-heading has-text-align-center" style="margin-bottom:var(--wp--preset--spacing--40);font-style:normal;font-weight:700;line-height:1.1">You have a vision. We have a team to get you there</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"11px"}}} -->
+<p class="has-text-align-center" style="font-size:11px">Contact Us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px"}}} -->
+<p class="has-text-align-center" style="font-size:16px">1-888-874-3791</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"11px"}}} -->
+<p class="has-text-align-center" style="font-size:11px">OR</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">SEND A MESSAGE</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
