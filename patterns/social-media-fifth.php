b739dcb37501706a729ea10f6e3184aba83422d3
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-02-15 09:32:34 +0545

    fifth socila media

--- patterns/social-media-fifth.php
+++ patterns/social-media-fifth.php
@@ -1,50 +1,50 @@
 <?php
 /**
- * Title: Social Media Fourth
- * Slug: featured/social-media-fourth
+ * Title: Social Media Fifth
+ * Slug: featured/social-media-fifth
  * Categories: featured
  */
 ?>
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"50px","bottom":"50px"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-group" style="padding-top:50px;padding-bottom:50px"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"center"} -->
-<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"align":"center","id":2924,"width":"350px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
-<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/media-4-1.png" alt="" class="wp-image-2924" style="width:350px;height:auto"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
+<div class="wp-block-columns"><!-- wp:column -->
 <div class="wp-block-column"><!-- wp:heading {"style":{"elements":{"link":{"color":{"text":"#253746"}}},"typography":{"fontSize":"36px","fontStyle":"normal","fontWeight":"800","lineHeight":"1"},"color":{"text":"#253746"}}} -->
-<h2 class="wp-block-heading has-text-color has-link-color" style="color:#253746;font-size:36px;font-style:normal;font-weight:800;line-height:1">Being Social is a full Time Job</h2>
+<h2 class="wp-block-heading has-text-color has-link-color" style="color:#253746;font-size:36px;font-style:normal;font-weight:800;line-height:1">Boost(ing ) Your Voice</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.4"},"color":{"text":"#253746"},"elements":{"link":{"color":{"text":"#253746"}}}}} -->
-<p class="has-text-color has-link-color" style="color:#253746;font-size:20px;font-style:normal;font-weight:400;line-height:1.4">Social media posting</p>
+<p class="has-text-color has-link-color" style="color:#253746;font-size:20px;font-style:normal;font-weight:400;line-height:1.4">Engage a larger audience </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"spacing":{"margin":{"top":"var:preset|spacing|60"}}},"textColor":"black"} -->
-<p class="has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--60);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Social media&nbsp;posts&nbsp;are—by their nature—temporary. That means you need to&nbsp;post&nbsp;regularly to stay relevant and in your followers’ minds. However, social media posting is only part of the equation.</p>
+<p class="has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--60);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">On networks like Facebook, only a small portion of your followers will see what you share. That’s where options like boosted posts can play an important role. Promote high-quality content like videos, blog posts or photos from your business to increase their visibility to people who like your page.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"default"}} -->
 <div class="wp-block-group"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"textColor":"black","className":"has-before-icon"} -->
-<p class="has-before-icon has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--30);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Professional social media posting </p>
+<p class="has-before-icon has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--30);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Expand your reach </p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"textColor":"black","className":"has-before-icon"} -->
-<p class="has-before-icon has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--30);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Organic growth</p>
+<p class="has-before-icon has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--30);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Promote high-quality content</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}},"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"textColor":"black","className":"has-before-icon"} -->
-<p class="has-before-icon has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--30);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Engage your audience </p>
+<p class="has-before-icon has-black-color has-text-color has-link-color" style="margin-top:var(--wp--preset--spacing--30);font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Increase Visibility</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group -->
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button -->
-<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">SOCIAL MEDIA PACKAGES</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">SOCIAL MEDIA PROMOTION</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:image {"align":"center","id":2954,"width":"350px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/media-5-1.jpg" alt="" class="wp-image-2954" style="width:350px;height:auto"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:group -->
