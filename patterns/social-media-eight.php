ede06cec1840aa6e6983aeddf91bb566e53307e5
Ramesh <ramesh.devkota@eaglevisionit.com>
2024-02-16 11:29:53 +0545

    fixed

---
+++ patterns/social-media-eight.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Title: Social Media Eight
+ * Slug: featured/social-media-eight
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"50px","bottom":"50px"}},"color":{"background":"#f3f3f3"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background" style="background-color:#f3f3f3;padding-top:50px;padding-bottom:50px"><!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"bottom":"35px"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-bottom:35px"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"30px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"},"color":{"text":"#433f34"},"elements":{"link":{"color":{"text":"#433f34"}}}}} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#433f34;font-size:30px;font-style:normal;font-weight:700;line-height:1.2">What is Social Media Management?</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"align":"center","id":2992,"width":"478px","height":"auto","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image aligncenter size-full is-resized"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/media-8-1.png" alt="" class="wp-image-2992" style="width:478px;height:auto"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|asset-6"}}}},"textColor":"black"} -->
+<p class="has-black-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:400;line-height:1.8"> <a href="#">Social media management</a> is an aspect of social media marketing that involves creating, scheduling and evaluating content posted across social media platforms. Social media marketing companies leverage various social media management tools and platforms, such as <a href="#">Hootsuite</a> and <a href="#">Onlypult</a>, to monitor your social media presence and bulk-schedule online posts.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<p class="has-black-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Given the growing population of consumers who encounter new brands every day via social media platforms, social media management is imperative to stay ahead of the curve. Social media management allows you to:</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50"}}}} -->
+<div class="wp-block-columns" style="padding-top:var(--wp--preset--spacing--50)"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"style":{"typography":{"lineHeight":"2.2"}}} -->
+<ul style="line-height:2.2"><!-- wp:list-item -->
+<li>Establish a unified brand</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Cut down business expenses</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Build up your customer relations</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:list {"style":{"typography":{"lineHeight":"2.2"}}} -->
+<ul style="line-height:2.2"><!-- wp:list-item -->
+<li>Take control of your online community</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Track your campaign development</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Increase your audience reach</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<p class="has-black-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:400;line-height:1.8">At Thrive, we understand that social media management is an ongoing process. That is why we stay abreast of the latest trends and market developments to unlock your brand’s full potential. Our social media marketing consultants utilize the latest technologies to manage your social media accounts and configure your campaign management processes to meet your specific needs and audience demands.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"400","lineHeight":"1.8"},"elements":{"link":{"color":{"text":"var:preset|color|asset-6"}}}},"textColor":"black"} -->
+<p class="has-black-color has-text-color has-link-color" style="font-size:16px;font-style:normal;font-weight:400;line-height:1.8">Stay on top of the evolving social media landscape and generate more business. <a href="#">Call us now</a> and let’s get started with your social media management campaign!</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
---
+++ patterns/social-media-seven.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Social Media Seven
+ * Slug: featured/social-media-seven
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"style":{"color":{"background":"#f3f3f3"},"spacing":{"padding":{"top":"var:preset|spacing|70","bottom":"var:preset|spacing|70"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-background" style="background-color:#f3f3f3;padding-top:var(--wp--preset--spacing--70);padding-bottom:var(--wp--preset--spacing--70)"><!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"30px","fontStyle":"normal","fontWeight":"700","lineHeight":"1.2"},"color":{"text":"#433f34"},"elements":{"link":{"color":{"text":"#433f34"}}}}} -->
+<h2 class="wp-block-heading has-text-align-center has-text-color has-link-color" style="color:#433f34;font-size:30px;font-style:normal;font-weight:700;line-height:1.2">Reach more people and grow your brand awareness</h2>
+<!-- /wp:heading -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","style":{"typography":{"fontSize":"20px","fontStyle":"normal","fontWeight":"400","lineHeight":"1"}}} -->
+<div class="wp-block-button has-custom-font-size" style="font-size:20px;font-style:normal;font-weight:400;line-height:1"><a class="wp-block-button__link has-text-align-center wp-element-button">LET'S CONNECT</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group -->
