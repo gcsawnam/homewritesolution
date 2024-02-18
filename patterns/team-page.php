869733040d9c59433235d42392cf403389a93420
gcsawnam <gcsawnam@gmail.com>
2024-02-18 13:24:35 +0545

    fixes

--- patterns/team-page.php
+++ patterns/team-page.php
@@ -5,3 +5,110 @@
  * Categories: featured
  */
 ?>
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:cover {"url":"http://localhost/wordpress/wp-content/uploads/2024/02/lukeandashley.com-61-Large.jpeg","id":7,"dimRatio":50,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span><img class="wp-block-cover__image-background wp-image-7" alt="" src="http://localhost/wordpress/wp-content/uploads/2024/02/lukeandashley.com-61-Large.jpeg" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Our Team</p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
+
+<!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|60","left":"2rem"},"margin":{"top":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-columns" style="margin-top:var(--wp--preset--spacing--40)"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"className":"fhs-team-box","layout":{"type":"constrained"}} -->
+<div class="wp-block-group fhs-team-box"><!-- wp:image {"id":8,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"custom","style":{"border":{"radius":"10px"}},"className":"is-style-fotawp-boxshadow-hover"} -->
+<figure class="wp-block-image size-full has-custom-border is-style-fotawp-boxshadow-hover"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/dipeshr-370x370-1.png" alt="" class="wp-image-8" style="border-radius:10px;aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"medium"} -->
+<h4 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-link-color has-medium-font-size" style="margin-top:var(--wp--preset--spacing--40);font-style:normal;font-weight:600">Anuj Shrestha</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Co-founder</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#545971","iconBackgroundColor":"transparent","iconBackgroundColorValue":"#ffffff00","align":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|20"},"margin":{"top":"0"}}}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color has-icon-background-color" style="margin-top:0"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"github"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"className":"fhs-team-box","layout":{"type":"constrained"}} -->
+<div class="wp-block-group fhs-team-box"><!-- wp:image {"align":"wide","id":9,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"custom","style":{"border":{"radius":"10px"}},"className":"is-style-fotawp-boxshadow-hover"} -->
+<figure class="wp-block-image alignwide size-full has-custom-border is-style-fotawp-boxshadow-hover"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/sachhi-di-370x370-1.png" alt="" class="wp-image-9" style="border-radius:10px;aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"medium"} -->
+<h4 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-link-color has-medium-font-size" style="margin-top:var(--wp--preset--spacing--40);font-style:normal;font-weight:600">Diya</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}},"fontSize":"small"} -->
+<p class="has-text-align-center has-small-font-size" style="margin-top:var(--wp--preset--spacing--30)">Content Writer</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#545971","iconBackgroundColor":"transparent","iconBackgroundColorValue":"#ffffff00","align":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|20"},"margin":{"top":"0"}}}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color has-icon-background-color" style="margin-top:0"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"github"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"className":"fhs-team-box","layout":{"type":"constrained"}} -->
+<div class="wp-block-group fhs-team-box"><!-- wp:image {"align":"wide","id":10,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"custom","style":{"border":{"radius":"10px"}},"className":"is-style-fotawp-boxshadow-hover"} -->
+<figure class="wp-block-image alignwide size-full has-custom-border is-style-fotawp-boxshadow-hover"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/Asheem-Manandhar-370x370-1.jpg" alt="" class="wp-image-10" style="border-radius:10px;aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"medium"} -->
+<h4 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-link-color has-medium-font-size" style="margin-top:var(--wp--preset--spacing--40);font-style:normal;font-weight:600"><a href="https://buddhahospital.com.np/doctor-details-4/" data-type="link" data-id="https://buddhahospital.com.np/doctor-details-4/"> Bimala Tiwari</a></h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}}} -->
+<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--30)">Developer</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#545971","iconBackgroundColor":"transparent","iconBackgroundColorValue":"#ffffff00","align":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|20"},"margin":{"top":"0"}}}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color has-icon-background-color" style="margin-top:0"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"github"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"className":"fhs-team-box","layout":{"type":"constrained"}} -->
+<div class="wp-block-group fhs-team-box"><!-- wp:image {"align":"wide","id":11,"aspectRatio":"3/4","scale":"cover","sizeSlug":"full","linkDestination":"custom","style":{"border":{"radius":"10px"}},"className":"is-style-fotawp-boxshadow-hover"} -->
+<figure class="wp-block-image alignwide size-full has-custom-border is-style-fotawp-boxshadow-hover"><img src="http://localhost/wordpress/wp-content/uploads/2024/02/Aakriti-Basnet-370x370-1.jpg" alt="" class="wp-image-11" style="border-radius:10px;aspect-ratio:3/4;object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}},"typography":{"fontStyle":"normal","fontWeight":"600"},"elements":{"link":{"color":{"text":"var:preset|color|primary"}}}},"textColor":"primary","fontSize":"medium"} -->
+<h4 class="wp-block-heading has-text-align-center has-primary-color has-text-color has-link-color has-medium-font-size" style="margin-top:var(--wp--preset--spacing--40);font-style:normal;font-weight:600"><a href="https://buddhahospital.com.np/doctor-details-5/" data-type="link" data-id="https://buddhahospital.com.np/doctor-details-5/">Caty L</a></h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}}}} -->
+<p class="has-text-align-center" style="margin-top:var(--wp--preset--spacing--30)">SEO</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:social-links {"iconColor":"foreground","iconColorValue":"#545971","iconBackgroundColor":"transparent","iconBackgroundColorValue":"#ffffff00","align":"center","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20","left":"var:preset|spacing|20"},"margin":{"top":"0"}}}} -->
+<ul class="wp-block-social-links aligncenter has-icon-color has-icon-background-color" style="margin-top:0"><!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"#","service":"github"} /-->
+
+<!-- wp:social-link {"url":"#","service":"linkedin"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
